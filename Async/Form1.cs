9d6fec1e61bfef6e02afaa792f5924f8eb709113
vikashIND <rkvikashraj@gmail.com>
2019-03-19 23:59:20 +0530

    Async and WebApp project added

---
+++ Async/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            DownloadHtml(@"http://localhost:24499/PdfRender.aspx");
+        }
+
+        private void DownloadHtml(string url)
+        {
+            var webClient = new WebClient();
+            //webClient.DownloadFile(url,@"c:\log\abc.pdf");
+
+            var bytes = webClient.DownloadData(url);
+            File.WriteAllBytes(@"c:\log\test1.pdf", bytes);
+            //var data = Encoding.ASCII.GetString(html);
+            //using(var streamWriter=new StreamWriter(@"c:\log\test.pdf"))
+            //{
+            //    streamWriter.Write(html);
+            //}
+        }
+    }
+}
---
+++ WebApp/PdfRender.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class PdfRender : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RenderPdf();
+        }
+
+        private void RenderPdf()
+        {
+            string FilePath = Server.MapPath("~/Docs/receipt.pdf");
+            WebClient User = new WebClient();
+            Byte[] FileBuffer = User.DownloadData(FilePath);
+            if (FileBuffer != null)
+            {
+                Response.ContentType = "application/pdf";
+                Response.AddHeader("content-length", FileBuffer.Length.ToString());
+                Response.BinaryWrite(FileBuffer);
+            }
+        }
+    }
+}
